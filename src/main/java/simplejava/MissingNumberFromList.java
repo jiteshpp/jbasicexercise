2488330e1c9b61a088a8b5815554650f46382b7d
Jitesh Pathak <Jitesh Pathak@DESKTOP-MUBC4CB>
2018-03-05 17:38:38 +0530

    Missing number from list

---
+++ src/main/java/simplejava/MissingNumberFromList.java
@@ -0,0 +1,35 @@
+package simplejava;
+
+import java.util.ArrayList;
+import java.util.List;
+/**
+ * 
+ * @author Jitesh Pathak
+ * This class can be used to find one missing number from arraylist 
+ */
+public class MissingNumberFromList {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		List<Integer> numbers = new ArrayList<Integer>(); 
+		
+		for (int i = 0 ; i<=100; i++)
+		{
+			numbers.add(i);
+		}
+		numbers.remove(45);
+		
+		
+		int result = (numbers.size()*(numbers.size()+1))/2;
+		int result1= 0;
+		for (int j : numbers)
+		{
+			result1= result1+j;
+		}
+		int missingnumber=result-result1;
+		System.out.println(missingnumber);
+		
+	}
+
+}
